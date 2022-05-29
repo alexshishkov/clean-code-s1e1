714b8341d9db30e55080ec75beef03be133125b7
Aleksej <Aleksejmocny@gmail.com>
2022-05-29 14:23:19 +0200

    feat: БЭМ

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new_item");//Add a new task.
+var taskInput=document.getElementById("new__item");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete_tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed_tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incomplete__tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("completed__tasks");//completed-tasks
 
 
 //New task list item
